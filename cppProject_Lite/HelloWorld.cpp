6a97f2a8facafb5710024eaab26502f5367492c3
LaiChao <619642846@qq.com>
2020-02-11 14:55:09 +0800

    delete odd vscode c++ project

--- cppProject_Lite/HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-// 这个文件夹的cpp文件是通过vscode编译运行的
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Hello World"<<endl;
-    cout<<"Hello VSCode"<<endl;
-    system("pause");
-    return 0;
-}
