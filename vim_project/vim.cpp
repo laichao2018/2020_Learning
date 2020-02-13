be2612c7399ef73834a20e0a5853740dc93cd588
LaiChao <619642846@qq.com>
2020-02-13 17:56:34 +0800

    add a leetcode topic

--- vim_project/vim.cpp
+++ vim_project/vim.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 using namespace std;
+int titleToNumber(string s){
+    if(s.empty()){
+        return 0;
+    }
+    int result=0;
+    for(int i=s.length();i>=0;i--){ //从后往前走
+        if(s[i]>='A'&&s[i]<='Z'){
+            result+=i*26+s[i]-'A'+1;
+        }
+    }
+    return result;
+}
+
+
 int main(){
-    cout<<"Hello World"<<endl;
+    
+    
     system("pause");
     return 0;
 }
