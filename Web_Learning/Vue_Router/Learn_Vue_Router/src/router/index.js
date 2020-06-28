7c6835ef786d4fc1fabd300ac64555c9e6daf0d0
LaiChao <619642846@qq.com>
2020-06-28 15:34:48 +0800

    modify router url

--- Web_Learning/Vue_Router/Learn_Vue_Router/src/router/index.js
+++ Web_Learning/Vue_Router/Learn_Vue_Router/src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+/// 配置路由相关信息
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+/// 通过vue.use（插件），安装插件
 Vue.use(Router)
 
 export default new Router({
+  //// routes配置路由和组件之间的应用关系
   routes: [
     {
       path: '/',
